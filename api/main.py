4632b668855c7b5a9cbda6f10332cc194111cc4f
Bryan Lima <42067040+BryanMLima@users.noreply.github.com>
2020-03-08 17:56:19 -0300

    Add files via upload

--- api/main.py
+++ api/main.py
@@ -1,3 +1,4 @@
+import os
 from hashcode import encrypt_string
 from flask import Flask, render_template, request
 from werkzeug.utils import secure_filename
@@ -14,11 +15,14 @@ def read():
     
 @app.route('/hashcode', methods = ['POST'])  
 def hashcode():  
-    if request.method == 'POST':  
+    if request.method == 'POST':
+        # Save file to read, then delete it
         f = request.files['file']  
         f.save(f.filename)
         file = open(f.filename, "r")
         content = file.read()
+        file.close()
+        os.remove(f.filename)
         hash_code = encrypt_string(content)
         return render_template("content.html", text = hash_code)
     
